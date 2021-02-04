423a53d3d7cf5f9a4fbaa960f3748b319ef8af52
Joe Shull <joseph.shull@gmail.com>
2021-02-04 16:36:03 -0700

    add multiple phone numbers

--- src/notifier.py
+++ src/notifier.py
@@ -13,18 +13,21 @@ client = Client(account_sid, auth_token)
 time_now = datetime.datetime.now().strftime('%Y%m%d%s')
 
 
-def easy_sms(msg, phone_number='+13038154080'):
-    if not isinstance(msg, str):
+def easy_sms(msg, phone_numbers=['+13038154080', '+18018147058']):
+    if not isinstance(msg, list):
         msg = str(msg)
-    if not isinstance(phone_number, str):
-        phone_number = str(phone_number)
-    message = client.messages.create(
-                                  from_='+13134838972',
-                                  body=msg,
-                                  to=phone_number
-                              )
+    if not isinstance(phone_numbers, list):
+        phone_numbers = [phone_numbers]
+    out = []
+    for phone_number in phone_numbers:
+        out.append(client.messages.create(
+                                      from_='+13134838972',
+                                      body=msg,
+                                      to=phone_number
+                                  )
+                    )
 
-    return message.sid
+    return out
 
 def main():
     print(easy_sms(f'this message sent from main {time_now}'))
